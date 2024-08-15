f96d1e0141ac180cc88111a105a6cd4d7351788c
Kathirvel <115524887+KATHIRVEL-13@users.noreply.github.com>
2024-08-15 17:11:23 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -5,9 +5,4 @@ sudo chmod 666 /var/run/docker.sock
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
-sudo apt install maven -y
-git clone https://github.com/KATHIRVEL-13/jpetstore-6.git
-cd /jpetstore-6
-mvn package
-docker build -t kathirvel13/petstore .
-docker run -d -p 8080:8080 kathirvel13/petstore
+docker run -d -p 8080:8080 adijaiswal/petsore
